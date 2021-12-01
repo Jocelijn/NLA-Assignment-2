c8c9d2917aa98f7946d2651b7e972ec4d5da191d
Jocelijn <jocelijn.cavero@gmail.com>
2021-12-01 12:12:15 +0100

    Update Excersise3.m

--- Excersise3.m
+++ Excersise3.m
@@ -1,94 +1,61 @@
-diag_vals = [0:10];
-A = diag(diag_vals);
-v = 2.^diag_vals';%ones(10,1);
-V = [v/norm(v)];
-H = [];
-[V,H] = ExtendArnoldi(A,V,H);
-A*V- V*H
-kAndResidual = []
-for k = [1:9]
-    sizeV = size(V)
-    size_H = size(H)
-    R = A*V(:,1:k)-V*H
-    
-    size_Hkk = size(H(1:k,1:k))
-    [W,D] = eig(H(1:k,1:k))
-    for j = [1:k]
-        
-        sizeW = size(W)
-        eigenVec = V(1:k,1:k)*W(:,j);
-        eigenVal = D(j,j);;
-        
-        r = norm(A*eigenVec );
-        %newPoint = [k; r];
-        %kAndResidual = [kAndResidual newPoint];
-    end
-    [V,H] = ExtendArnoldi(A,V,H);
+function ex3(A,v,k) 
+%note output has been changed from the original code
+T = zeros(k+1,k);
+
+v = v/norm(v);      V = v;
+w = A*v;            a(1) = v'*w;
+w = w - v*a(1);     b(2) = norm(w);
+v = w/b(2);         V = [V v];
+
+%forming of first column of T
+t = [a(1) b(2) zeros(1,k-1)];
+T(:,1) = t';
+
+%solving eigenvalue problem
+[y,mu] = eig(T(1,1));
+Mu=[1];
+Mu = [Mu, mu(1,1)];
+r = [abs(T(2,1))*abs(y(1,1))];
+
+Table=[Mu; T(k+1,k) r];
+Plot=zeros(k,k);
+Plot(1,1)=Table(2,2);
+
+for j=2:k
+    r = [];
+%Lanczos iteration
+w = A*v;
+a(j) = v'*w;
+w = w - b(j)*V(:,j-1)-a(j)*v;
+b(j+1) = norm(w);
+v = w/b(j+1);
+V = [V v];  
+
+%forming of rest of the columns of T
+t = [zeros(1,j-2) b(j) a(j) b(j+1) zeros(1,k-j)];
+T(:,j) = t';
+
+%solving eigenvalue problem
+[y,mu] = eig(T(1:j,1:j));
+Mu=[j];
+for i=1:j
+    Mu = [Mu, mu(i,i)];
+    r = [r,abs(T(i+1,i))*abs(y(i,i))];
+end
+Table=[Mu ; T(j+1,j) r];
+if j==k
+    Table=[Mu ; 0 r];
+end
+disp(Table)
+
+for n=1:j
+    Plot(j,n)=Table(2,n+1);
 end
 
-plot(kAndResidual)
-
-function [W,H2] = FilterAway(mu,V,H)
-size_h = size(H);
-k = size_h(2);
-[Q,R] =  qr(H- mu*eye(k+1,k),0);
-W = V*Q;
-
-H2 = R*Q(1:k,1:k-1)+mu*eye(k,k-1);
 end
 
-function [V2,H2] = ExtendArnoldi(A,V,H)
-    v = V(:,end);
-    w = A*v;   
-    q = w;
-    for l = 1:size(V,2)
-        w = w-V(:,l)*(V(:,l)'*w);
-    end
-    g = norm(w);
-    
-    v = w/g;
-    h = V'*q;
-    
-    V2 = [V v];
-    H
-    H2 = [H,h; zeros(1,size(V,2)-1),g];
-end
-
-function L = ListRitzData(H)
-    size_H = size(H);
-    [V,D] = eig(H(1:size_H(2),1:size_H(2)));
-    k = size_H(2);
-    h = H(k+1,k);
-    ResultTable = [k; abs(h)];
-    for j = 1 : k
-        eigenVec = V(:,j);
-        r = abs(h*eigenVec(k));
-        eigenVal = D(j,j);
-        
-        newColumn = [eigenVal;r];
-        ResultTable = [ResultTable newColumn];
-    end
-    display(ResultTable)
-    L =  diag(D);
-end
-
-function [V,H] = ArnoldiMGS(A,v,k)
-v = v/norm(v); 
-V = v; H = [];
-n = size(v);
-
-for j=1:k
-    w = A*v;   
-    q = w;
-    for l = 1:size(V,2)
-        w = w-V(:,l)*(V(:,l)'*w);
-    end
-    g = norm(w);
-    
-    v = w/g;
-    h = V'*q;
-    
-    V = [V v];
-    H = [H,h; zeros(1,j-1),g];
-end
+color = rand(k,3);
+for t=1:k
+    plot(t,Plot(t,1:t), '-*','MarkerEdgeColor', color(t,:));
+    hold on;
 end
